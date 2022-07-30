d7cba2652982440d78ef6345ec321edff5aa36ca
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-07-30 03:25:52 +0000

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -1,7 +1,7 @@
 def main():
-    user=input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
+    user=input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
 
-    if user =="42" or user =="forty-two " or user =="forty two":
+    if user =="42" or user =="forty-two" or user =="forty two":
         print("Yes")
 
     else:
