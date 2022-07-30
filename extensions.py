70548cda59b260efc55f65198e5117f7d932a297
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-07-30 03:45:27 +0000

    automated commit by check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -10,6 +10,17 @@ def main():
     elif file.endswith(".jpg") or file.endswith(".jpeg"):
         print("image/jpeg")
 
+    elif file.endswith(".png"):
+        print("image/png")
+
+    elif file.endswith(".txt"):
+        print("text/plain")
+
+    elif file.endswith(".zip"):
+        print("application/zip")
+    else:
+        print("application/octet-stream")
+
 
 
 
