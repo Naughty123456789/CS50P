e1b1245b25e78e72e17dd9a0bf43826947126ff2
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-07-30 15:31:33 +0000

    automated commit by check50 [check50=True]

--- taqueria.py
+++ taqueria.py
@@ -1,20 +1,20 @@
 def main():
 
-    d={"Baja Taco": 4.00,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00,
+    d={"baja taco": 4.00,
+    "burrito": 7.50,
+    "bowl": 8.50,
+    "nachos": 11.00,
+    "quesadilla": 8.50,
+    "super burrito": 8.50,
+    "super quesadilla": 9.50,
+    "taco": 3.00,
+    "tortilla salad": 8.00,
     }
 
     total=0
     while True:
         try:
-            item=input("Item:").strip()
+            item=input("Item:").strip().lower()
             if item in d:
                     total+=d[item]
                     print("Total: $",end="")
