e79f6e72761f114b4243f0847dc93b7926b371a4
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-07-31 03:32:30 +0000

    automated commit by check50 [check50=True]

---
+++ outdated.py
@@ -0,0 +1,59 @@
+def main():
+    while True:
+        try:
+            date=input("Date:").strip()
+
+            if "/" in date:
+                x,y,z=date.split("/")
+                a=int(x)
+                b=int(y)
+                c=int(z)
+
+
+
+
+
+
+
+
+            elif "," in date:
+                x,y,z=date.split(" ")
+
+                c=int(z)
+                b=int(y.replace(",",""))
+
+                mylist=[
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"]
+                a=0
+                for i in range(len(mylist)):
+                    if mylist[i]==x:
+                        a=i+1
+
+            if a<1 or a>12:
+                raise UnboundLocalError
+            if b<1 or b>31:
+                raise UnboundLocalError
+            print(f'{c}-{a:02}-{b:02}')
+            break
+
+        except UnboundLocalError:
+            print("Wrong format")
+            pass
+
+
+
+
+
+
+main()
