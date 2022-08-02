a56b4efcfb15899335cfa69445fd196d8cff23b7
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-02 03:13:12 +0000

    automated commit by check50 [check50=True]

--- test_twttr.py
+++ test_twttr.py
@@ -2,11 +2,19 @@ from twttr import shorten
 
 def main():
     checking_vowels_and_caps()
+    checking_num()
+    checking_punct()
 
 def checking_vowels_and_caps():
     assert shorten("Twitter")=="Twttr"
     assert shorten("Bittergourd")=="Bttrgrd"
     assert shorten("Immediately")=="mmdtly"
+
+def checking_num():
+    assert shorten("1234")=="1234"
+
+def checking_punct():
+    assert shorten("?!,.")=="?!,."
 
 
 
