cebd58bdda55cd575744b1852f7b17f30c1b9e62
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-03 01:26:30 +0000

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -1,35 +1,23 @@
 from bank import value
 
 
-
 def main():
-    test_0dollars()
-    test_20dollars()
-    test_100dollars()
+    test_zerodollars()
+    test_twentydollars()
+    test_hundreddollars()
 
-
-
-def test_0dollars():
+def test_zerodollars():
     assert value("hello")=="$0"
     assert value("Hello")=="$0"
 
 
-def test_20dollars():
+def test_twentydollars():
     assert value("hi")=="$20"
     assert value("horror")=="$20"
 
-def test_100dollars():
+def test_hundreddollars():
     assert value("I am hungry")=="$100"
     assert value("Pizza")=="$100"
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
