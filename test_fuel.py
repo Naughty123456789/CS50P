f0c1e4ce612c5fc0b0e729e8e3d8724abf2e2d84
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-03 13:24:58 +0000

    automated commit by check50 [check50=True]

---
+++ test_fuel.py
@@ -0,0 +1,27 @@
+from fuel import convert, gauge
+import pytest
+
+def main():
+    test_zero_division()
+    test_value_error()
+    test_correct_input()
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+def test_value_error():
+    with pytest.raises(ValueError):
+        convert("three/four")
+
+
+def test_correct_input():
+    assert convert("1/4")==25 and gauge(25)=="25%"
+    assert convert("3/5")==60 and gauge(60)=="60%"
+
+
+
+
+if __name__ == "__main__":
+    main()
