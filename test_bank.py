8f2d198d4355c1b8292f6d1338a3167202383055
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-03 02:38:14 +0000

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -6,18 +6,17 @@ def main():
     test_hundreddollars()
 
 def test_zerodollars():
-    assert value("hello")=="0"
-    assert value("Hello")=="0"
+    assert value("hello")==0
+    assert value("Hello")==0
 
 
 def test_twentydollars():
-    assert value("hi")=="20"
-    assert value("horror")=="20"
-    assert value("heeellloo")=="20"
+    assert value("hi")==20
+    assert value("horror")==20
 
 def test_hundreddollars():
-    assert value("I am hungry")== "100"
-    assert value("Pizza")== "100"
+    assert value("I am hungry")== 100
+    assert value("Pizza")== 100
 
 
 if __name__ == "__main__":
