0fcac2ec30ee74397a51a2d421744cdba63a834d
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-05 03:50:07 +0000

    automated commit by check50 [check50=True]

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -3,6 +3,7 @@ from numb3rs import validate
 def main():
     test_range()
     test_alphabet()
+    test_special_case()
 
 
 def test_range():
@@ -11,8 +12,12 @@ def test_range():
     assert validate("127.0.0.1")==True
     assert validate("512.512.512.512")==False
 
+
 def test_alphabet():
     assert validate("cat")==False
+
+def test_special_case():
+    assert validate("233....")==False
 
 
 if __name__ == "__main__":
