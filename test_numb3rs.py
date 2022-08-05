7d4475f5c895ebbc9c000b783c1c25db20d0c087
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-05 03:55:19 +0000

    automated commit by check50 [check50=True]

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -3,21 +3,24 @@ from numb3rs import validate
 def main():
     test_range()
     test_alphabet()
-    test_special_case()
+    test_format()
 
 
 def test_range():
-    assert validate("255.255.255.255")==True
-    assert validate("1.2.3.1000")==False
-    assert validate("127.0.0.1")==True
-    assert validate("512.512.512.512")==False
+    assert validate(r"255.255.255.255")==True
+    assert validate(r"1.2.3.1000")==False
+    assert validate(r"127.0.0.1")==True
+    assert validate(r"512.512.512.512")==False
 
 
 def test_alphabet():
     assert validate("cat")==False
 
-def test_special_case():
+def test_format():
     assert validate(r"1")==False
+    assert validate(r"1.2")==False
+    assert validate(r"1.2.3")==False
+    assert validate(r"1.2.3.4")== True
 
 
 if __name__ == "__main__":
