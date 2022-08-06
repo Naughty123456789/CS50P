56e28aaa76c7a6ee3cc9ba23e2254c08a1c6bdf4
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-06 01:45:05 +0000

    automated commit by check50 [check50=True]

--- watch.py
+++ watch.py
@@ -8,7 +8,7 @@ def main():
 
 def parse(s):
     if re.search(r"<iframe(.)*><\/iframe>",s):
-        new_pattern=re.search(r"(http(s)*):\/\/(www\.)*youtube\.com\/embed\/[a-zA-Z_0-9]+",s)
+        new_pattern=re.search(r"(http(s)*:\/\/(www\.)*youtube\.com\/embed\/)([a-zA-Z_0-9]+)",s)
         if new_pattern:
             final_pattern=new_pattern.group()
             return final_pattern
