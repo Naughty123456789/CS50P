08b1060ec322c369f61d710989511115189972e2
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-06 04:30:07 +0000

    automated commit by check50 [check50=True]

--- test_um.py
+++ test_um.py
@@ -10,18 +10,17 @@ def main():
 
 
 def test_special_cases():
-    assert count("Um, may i um see your album")== 2
-    assert count("Big bum")==0
-    assert count("UMMM")==1
+    assert count("Um, may i see your album")== 1
+    assert count("yummy")==0
+    assert count("UMMM")==0
 
 def test_normal():
-    assert count("um")==1
+    assert count("Hello um World")==1
     assert count("um?")==1
 
 def test_caps():
-    assert count("Um, thanks")==1
-    assert count("Um Um")==2
-
+    assert count("Um, thanks,um...")==2
+    
 
 
 
