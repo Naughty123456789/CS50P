3c247edbe18b73557c9e8e5f4e9303d06cffaaed
Naughty123456789 <Naughty123456789@users.noreply.github.com>
2022-08-06 04:16:21 +0000

    automated commit by check50 [check50=True]

---
+++ um.py
@@ -0,0 +1,16 @@
+import re
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    um_list=re.findall(r"\b\W*((u){1}(m){1})\W*",s, re.IGNORECASE)
+    return len(um_list)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
